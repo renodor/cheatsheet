68976501db7338d15b87d1b3af906aa6f8b9e974
renaud dor <renaud.dor@gmail.com>
2020-10-01 19:10:17 -0500

    Improve navbar JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,12 +8,29 @@ const closeAllSubSections = () => {
   });
 }
 
+const openSubSection = (subSection) => {
+  const subSectionHeight = subSection.firstElementChild.clientHeight;
+  subSection.style.height = `${subSectionHeight}px`;
+}
+
+const closeSubSection = (subSection) => {
+  subSection.style.height = 0;
+}
+
+
 sectionLinks.forEach((sectionLink) => {
+  if (sectionLink.dataset.isOpen == 'true') {
+    openSubSection(sectionLink.nextElementSibling);
+  }
+
   sectionLink.addEventListener('click', (event) => {
-    closeAllSubSections();
-    const subSection = event.currentTarget.nextElementSibling;
-    const subSectionHeight = subSection.firstElementChild.clientHeight;
-    subSection.style.height = `${subSectionHeight}px`;
-    sectionLink.dataset.isOpen = true;
+    if (sectionLink.dataset.isOpen == 'false') {
+      closeAllSubSections();
+      openSubSection(event.currentTarget.nextElementSibling);
+      sectionLink.dataset.isOpen = true;
+    } else if (sectionLink.dataset.isOpen == 'true') {
+      closeSubSection(event.currentTarget.nextElementSibling);
+      sectionLink.dataset.isOpen = false;  
+    }
   })
 })
